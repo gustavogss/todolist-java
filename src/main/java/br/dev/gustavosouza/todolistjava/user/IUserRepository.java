eca451b0cc96161871318b5e0bbcbf524dac3ac5
*G3u0g0a00# <gustavogss.dev@gmail.com>
2023-10-10 15:49:41 -0300

    feat: repository interface

---
+++ src/main/java/br/dev/gustavosouza/todolistjava/user/IUserRepository.java
@@ -0,0 +1,11 @@
+package br.dev.gustavosouza.todolistjava.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+    UserModel findByUsername(String username);
+}
--- src/main/java/br/dev/gustavosouza/todolistjava/user/UserController.java
+++ src/main/java/br/dev/gustavosouza/todolistjava/user/UserController.java
@@ -1,9 +1,11 @@
 package br.dev.gustavosouza.todolistjava.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());      
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if(user != null){       
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);   
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     } 
 }
